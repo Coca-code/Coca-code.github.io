6203519db3b0c399a10fa31fcdf52232025199ec
殷敏峰 <ymf_wx1981@hotmail.com>
2017-03-05 02:35:20 +0800

    ----- Modified -----

--- jsFolder/index.js
+++ jsFolder/index.js
@@ -1,19 +1,29 @@
-$(document).ready(function(){
-	console.log("----- index.html run! -----");
-	//-----------
-	//变量&DOM
-	//-----------
-	var oDocument = $("document"),
-	    oBody = $("body");
-	var oLoading = $(".loading");
+$(document).ready(function() {
+    console.log("----- index.html run! -----");
+    //-----------
+    //变量&DOM
+    //-----------
+    var oLoading = $(".loading");
+    var oBody = $(".c-body");
+    var oBtnPrint = $(".print");
 
-	    oBody.click(function(event) {
-	    	/* Act on the event */
-	    	var event = window.event || event;
-	    	event.preventDefault();
-	    	console.log("Body clicked!");
-	    });
+    //打印函数
+    function printHtml(html) {
+        var bodyHtml = document.body.innerHTML;
+        document.body.innerHTML = html;
+        window.print();
+        document.body.innerHTML = bodyHtml;
+    };
+
+        oBody.click(function(event) {
+        /* Act on the event */
+        var event = window.event || event;
+        event.preventDefault();
+        console.log("Body clicked!");
+    });
 
 
-	  oLoading.fadeOut(600);
+
+
+    oLoading.fadeOut(600);
 });
