392363893fe45ccf59097f91db4d246872e08ee7
殷敏峰 <ymf_wx1981@hotmail.com>
2018-02-10 23:37:02 +0800

    ---- Updated @2018 ----

--- jsFolder/index.js
+++ jsFolder/index.js
@@ -26,4 +26,4 @@
 
 //     oLoading.fadeOut(600);
 // });
-$(document).ready(function(){console.log("----- index.html run! -----");var d=$(".loading");var c=$(".c-body");var a=$(".print");function b(f){var e=document.body.innerHTML;document.body.innerHTML=f;window.print();document.body.innerHTML=e}a.click(function(f){var f=window.event||f;f.preventDefault();var e=c.html();b(e)});d.fadeOut(600)});
+$(document).ready(function(){console.log("----- index Mark.yin @2018 -----");var d=$(".loading");var c=$(".c-body");var a=$(".print");function b(f){var e=document.body.innerHTML;document.body.innerHTML=f;window.print();document.body.innerHTML=e}a.click(function(f){var f=window.event||f;f.preventDefault();var e=c.html();b(e)});d.fadeOut(600)});
