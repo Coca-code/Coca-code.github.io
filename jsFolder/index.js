ca93bfa6dbfbf12c0f9eb9aaa9393e73095b2aac
殷敏峰 <ymf_wx1981@hotmail.com>
2018-04-28 04:06:50 +0800

    UPDATED AT 2018

--- jsFolder/index.js
+++ jsFolder/index.js
@@ -4,12 +4,5 @@ $(document).ready(function() {
     //变量&DOM
     //-----------
     var oLoading = $(".loading");
-
-
-
-
     oLoading.fadeOut(600);
 });
-
-
-//$(document).ready(function(){var d=$(".loading");var c=$(".c-body");var a=$(".print");function b(f){var e=document.body.innerHTML;document.body.innerHTML=f;window.print();document.body.innerHTML=e}a.click(function(f){var f=window.event||f;f.preventDefault();var e=c.html();b(e)});d.fadeOut(600)});
